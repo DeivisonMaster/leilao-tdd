bf18300dc03282a5822a2ba564859ab335d16e79
Deivison Oliveira <deivisonoliveira.info@gmail.com>
2022-04-19 21:54:41 -0400

    automatizando e criando novos testes de unidade

--- src/br/com/caelum/leilao/teste/TesteAvaliador.java
+++ src/br/com/caelum/leilao/teste/TesteAvaliador.java
@@ -3,6 +3,7 @@ package br.com.caelum.leilao.teste;
 import static org.junit.Assert.assertEquals;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -22,21 +23,63 @@ public class TesteAvaliador {
 		
 		Leilao leilaoDeVeiculos = new Leilao("Carros usados");
 		leilaoDeVeiculos.propoe(new Lance(joao, new BigDecimal("300.0")));
-		leilaoDeVeiculos.propoe(new Lance(jose, new BigDecimal("100.0")));
 		leilaoDeVeiculos.propoe(new Lance(jose, new BigDecimal("400.0")));
 		leilaoDeVeiculos.propoe(new Lance(maria, new BigDecimal("250.0")));
-		leilaoDeVeiculos.propoe(new Lance(joao, new BigDecimal("550.0")));
-		leilaoDeVeiculos.propoe(new Lance(maria, new BigDecimal("10.0")));
 		
 		// ação
 		Avaliador avaliador = new Avaliador();
 		avaliador.avaliaLances(leilaoDeVeiculos);
 		
 		// validação
-		BigDecimal maiorEsperado = new BigDecimal("550.0");
+		BigDecimal maiorEsperado = new BigDecimal("400.0");
 		assertEquals(maiorEsperado, avaliador.getMaiorLance());
 		
-		BigDecimal menorEsperado = new BigDecimal("10.0");
+		BigDecimal menorEsperado = new BigDecimal("250.0");
 		assertEquals(menorEsperado, avaliador.getMenorLance());
 	}
+	
+	@Test
+	public void deveEntenderLeilaoComApenasUmLance() {
+		Usuario joao = new Usuario("João");
+		
+		Leilao leilaoVeiculo = new Leilao("Leilão de Veiculos");
+		leilaoVeiculo.propoe(new Lance(joao, new BigDecimal("1000.0")));
+		
+		Avaliador avaliador = new Avaliador();
+		avaliador.avaliaLances(leilaoVeiculo);
+		
+		BigDecimal maiorEsperado = new BigDecimal("1000.0");
+		assertEquals(maiorEsperado, avaliador.getMaiorLance());
+		
+		BigDecimal menorEsperado = new BigDecimal("1000.0");
+		assertEquals(menorEsperado, avaliador.getMenorLance());
+	}
+	
+	@Test
+	public void deveEncontrarOsTresMaioresLances() {
+		Usuario joao = new Usuario("João");
+		Usuario maria = new Usuario("Maria");
+		Usuario jose = new Usuario("José");
+		
+		Leilao leilaoDeVeiculos = new Leilao("Carros usados");
+		leilaoDeVeiculos.propoe(new Lance(joao, new BigDecimal("900.0")));
+		leilaoDeVeiculos.propoe(new Lance(jose, new BigDecimal("400.0")));
+		leilaoDeVeiculos.propoe(new Lance(maria, new BigDecimal("130.0")));
+		leilaoDeVeiculos.propoe(new Lance(maria, new BigDecimal("1000.0")));
+		leilaoDeVeiculos.propoe(new Lance(jose, new BigDecimal("750.0")));
+		
+		Avaliador avaliador = new Avaliador();
+		avaliador.avaliaLances(leilaoDeVeiculos);
+		
+		List<Lance> tresMaioresLances = avaliador.getTresMaioresLances();
+		assertEquals(3, tresMaioresLances.size());
+		assertEquals(new BigDecimal("1000.0"), tresMaioresLances.get(0).getValor());
+		assertEquals(new BigDecimal("900.0"), tresMaioresLances.get(1).getValor());
+		assertEquals(new BigDecimal("750.0"), tresMaioresLances.get(2).getValor());
+		
+		tresMaioresLances.forEach(lance -> {
+			System.out.println(lance.getValor());
+		});
+	}
+	
 }
--- src/br/com/leilao/servico/Avaliador.java
+++ src/br/com/leilao/servico/Avaliador.java
@@ -1,12 +1,18 @@
 package br.com.leilao.servico;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
+import br.com.leilao.dominio.Lance;
 import br.com.leilao.dominio.Leilao;
 
 public class Avaliador {
 	private BigDecimal maiorLance = new BigDecimal("-1");
-	private BigDecimal menorLance = new BigDecimal("999");
+	private BigDecimal menorLance = new BigDecimal("99999");
+	private List<Lance> maiores;
 
 	public void avaliaLances(Leilao leilao) {
 		leilao.getLances().forEach(item -> {
@@ -17,6 +23,19 @@ public class Avaliador {
 				menorLance = item.getValor();
 			}
 		});
+		
+		maiores = new ArrayList<>(leilao.getLances());
+		Collections.sort(maiores, new Comparator<Lance>() {
+			@Override
+			public int compare(Lance l1, Lance l2) {
+				if(l1.getValor().compareTo(l2.getValor()) < 1)
+					return 1;
+				if(l1.getValor().compareTo(l2.getValor()) >= 1)
+					return -1;
+				return 0;
+			}
+		});
+		maiores = maiores.subList(0, maiores.size() > 3 ? 3 : maiores.size());
 	}
 	
 	public BigDecimal getMaiorLance() {
@@ -25,5 +44,9 @@ public class Avaliador {
 	
 	public BigDecimal getMenorLance() {
 		return menorLance;
+	}
+	
+	public List<Lance> getTresMaioresLances() {
+		return maiores;
 	}
 }
